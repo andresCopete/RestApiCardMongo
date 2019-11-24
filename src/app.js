ccb263fee2b3a825d0769f3ece55dd7b6f9ca84a
andrecsc <andresf.l.c10@gmail.com>
2019-11-23 20:05:10 -0500

    Validacion de Parametros de conexion mongoose

--- src/app.js
+++ src/app.js
@@ -1,6 +1,14 @@
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
 
+mongoose.connect('mongodb://localhost/restAPI',{useNewUrlParser: true, useUnifiedTopology: true})
+.then(db=>{console.log('base de datos conectada')})
+.catch(err=>{console.log('Error al conectar a base de datos', err)})
+app.use(morgan('dev'));
+app.use(bodyParser.json());
 app.set('port', process.env.PORT || 3000);
 
 app.listen(app.get('port'), ()=>{
