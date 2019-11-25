4601082cb27df6b57854b895c3c4805174d39133
andrecsc <andresf.l.c10@gmail.com>
2019-11-25 15:49:34 -0500

    Validacion  de rutas en servicio app

--- src/app.js
+++ src/app.js
@@ -4,6 +4,8 @@ const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
 
+
+const userRouter = require('./routes/users');
 mongoose.connect('mongodb://localhost/restAPI',{useNewUrlParser: true, useUnifiedTopology: true})
 .then(db=>{console.log('base de datos conectada')})
 .catch(err=>{console.log('Error al conectar a base de datos', err)})
@@ -11,6 +13,11 @@ app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.set('port', process.env.PORT || 3000);
 
+
+//routes
+app.use('/users', userRouter);
+
+//static files
 app.listen(app.get('port'), ()=>{
     console.log('Servidor Iniciado', app.get('port'))
 });
